bb3026d2fea3adab2b22fc7dec565282d00927fb
libra1919 <aymentli@gmail.com>
2020-05-09 19:57:04 +0200

    first commit

---
+++ database/index.js
@@ -0,0 +1,7 @@
+const mongoose = require ('mongoose');
+
+
+mongoose.connect('mongodb+srv://libro:libra@cluster0-4guqr.mongodb.net/twitter?retryWrites=true&w=majority',{ 
+        useNewUrlParser: true, useUnifiedTopology: true
+ }).then(()=>console.log('connexion OK !'
+)).catch(err=>console.log('error') );
