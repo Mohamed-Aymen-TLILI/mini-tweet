dac89f220fe888a15dc0d70d3473ffd0fbc702a7
Mohamed Aymen TLILI <45849423+Mohamed-Aymen-TLILI@users.noreply.github.com>
2020-05-30 23:02:34 +0200

    Update developpement.js

--- environnement/developpement.js
+++ environnement/developpement.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  dbUrl: 'mongodb+srv://libro:libra@cluster0-4guqr.mongodb.net/twitter?retryWrites=true&w=majority',
+  dbUrl: 'mongodb+srv://****@cluster0-4guqr.mongodb.net/twitter?retryWrites=true&w=majority',
   cert: path.join( __dirname, '../ssl/local.crt'),
   key: path.join( __dirname, '../ssl/local.key'),
   portHttp: 3000,
